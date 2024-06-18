bd73cef9343b217318e62776e622246c7a17135c
Liann2 <escondekhielian@gmail.com>
2024-06-18 18:40:12 +0800

    Add client front-end pages and resources.

---
+++ src/main/java/com/example/medisync/Login.java
@@ -0,0 +1,31 @@
+package com.example.medisync;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Login extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("LogIn.fxml"));
+            Scene scene = new Scene(root, 1280, 960);
+            primaryStage.setTitle("Hello!");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/com/example/medisync/Register.java
@@ -0,0 +1,32 @@
+package com.example.medisync;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Register extends Application {
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("Register.fxml"));
+            Scene scene = new Scene(root, 1280, 960);
+            primaryStage.setTitle("Hello!");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
